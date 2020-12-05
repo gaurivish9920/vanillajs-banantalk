9cf6296b2d0d53d21845ff2c5342ad96cb77ed02
gaurivish9920 <75183205+gaurivish9920@users.noreply.github.com>
2020-12-05 18:51:46 +0530

    import js

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log("Script is working from different source.")
+// console.log("Script is working from different source.")
+var username = prompt("Give me your name: ")
+var welcomemess = "Welcome, this script worksga" + username
+alert(welcomemess)
