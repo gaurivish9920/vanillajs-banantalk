3bd83d9bddafaf0bfecbd00ed11cf333a448a4a3
gaurivish9920 <75183205+gaurivish9920@users.noreply.github.com>
2020-12-05 23:06:58 +0530

    updates

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var btnTranslate = document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-Input")
+var outputTrans = document.querySelector("#output")
 
 console.log(txtInput)
 function clickHandler() {
