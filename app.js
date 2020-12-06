0a35afba6a2d0a2e3f9b23e50ec47db0927b9a6e
gaurivish9920 <75183205+gaurivish9920@users.noreply.github.com>
2020-12-06 19:56:56 +0530

    input output processing

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var btnTranslate = document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-Input")
 var outputTrans = document.querySelector("#output")
 
-console.log(txtInput)
+// outputTrans.innerText="hihihihhi"  ----------------- .innerText is used to show output
+
+
 function clickHandler() {
-    console.log("clicked!");
-    console.log("input" , txtInput.value);
+    outputTrans.innerText = "smdineojvjvnv " + txtInput.value;
 };
+// .value is used to read values given as input  
 
 btnTranslate.addEventListener("click", clickHandler)
 
