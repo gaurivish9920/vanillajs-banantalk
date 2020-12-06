cc95ad7ed108347c08ec9ef46d83fc4ead30c9d5
gaurivish9920 <75183205+gaurivish9920@users.noreply.github.com>
2020-12-06 21:23:22 +0530

    update

--- app.js
+++ app.js
@@ -4,9 +4,29 @@ var outputTrans = document.querySelector("#output")
 
 // outputTrans.innerText="hihihihhi"  ----------------- .innerText is used to show output
 
+var serverurl = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text) {
+    return serverurl + "?" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured!",error)
+    alert("somethins is wrong with the server! Try again after sometime!")
+}
+
 
 function clickHandler() {
-    outputTrans.innerText = "smdineojvjvnv " + txtInput.value;
+    // outputTrans.innerText = "smdineojvjvnv " + txtInput.value;
+    var inputText= txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response=>response.json())
+    .then(json => {
+        var translatedText= json.contents.translated;
+        outputTrans.innerText= translatedText;
+    })
+    .catch(errorHandler)
 };
 // .value is used to read values given as input  
 
